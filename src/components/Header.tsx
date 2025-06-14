892b975e3b916b0b9ed3c1b00c7bb3e35430ac17
jpea-dev <notebook.jpea@gmail.com>
2025-06-14 08:27:32 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -20,7 +20,7 @@ const Header: React.FC = () => {
                 <img 
                   src={siteConfig.media.logo}
                   alt={`${siteConfig.site.name} Logo`}
-                  className="h-16 w-16 sm:h-20 sm:w-20 border-4 border-primary-200 object-cover"/>
+                  className="h-20 w-20 sm:h-20 sm:w-20 object-cover"/>
               </div>
               <div>
                 <h1 className="text-2xl sm:text-4xl lg:text-6xl font-bold text-primary-900 font-serif tracking-tight">
