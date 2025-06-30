1f9cffbb0ff523344ea697c95ae1c183450439c3
jpea-dev <notebook.jpea@gmail.com>
2025-06-30 13:24:06 +0530

    Update MandatoryDisclosure.tsx

--- src/pages/MandatoryDisclosure.tsx
+++ src/pages/MandatoryDisclosure.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const MandatoryDisclosure: React.FC = () => (
   <div style={{ height: '100vh' }}>
     <embed
-      src="/mandatory-disclosure.pdf" // path to your PDF in the public folder
+      src="/mpd_saras.pdf" // path to your PDF in the public folder
       type="application/pdf"
       width="100%"
       height="100%"
